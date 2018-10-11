5cb84c5c48c91c9652e3d0f13edccb4a5e42d0e2
discmagnet <kkumbier13@gmail.com>
2018-10-11 01:37:53 -0400

    includes kernel smoothing function

--- hw2.R
+++ hw2.R
@@ -1,10 +1,11 @@
-# Problem 1
+# Problem 1 -----------------------------------------------------------------------
 
 library(haven)
 library(dplyr)
 library(ggfortify)
 library(survival)
 library(ggplot2)
+library(broom)
 
 anemia2 <- read_sas("~/WORKING_DIRECTORIES/biostat.675/anemia2.sas7bdat")
 
@@ -96,3 +97,47 @@ plot09 <- autoplot(fit_cm, conf.int = FALSE, surv.connect = FALSE) +
 plot07
 plot08
 plot09
+
+# (c) Carry out a Logrank Test of CSP and MTX, but stratified on age group.
+
+# Logrank Test based on CSP_MTX (rho set to 0)
+LRT_csp_str01 <- survdiff(Surv(obs_time,GVHD) ~ CSP_MTX, data = anemia2, rho = 0,
+                    subset = (under20 == 1))
+# Logrank Test based on CSP_MTX (rho set to 0)
+LRT_csp_str02 <- survdiff(Surv(obs_time,GVHD) ~ CSP_MTX, data = anemia2, rho = 0,
+                    subset = (under20 == 0))
+
+# Problem 2 -----------------------------------------------------------------------
+
+anemia2 <- arrange(anemia2, obs_time)
+anemia2$atRisk <- c(64:59,59,57,56,55,55,53,53,rep(51,4),47,47,45:24,rep(23,23))
+anemia2 <- mutate(anemia2, hazard = GVHD/atRisk) # compute hazard function
+
+b <- 4 # specify desired bandwidth
+# plot smoothed hazard estimator for b < t < t_n - b
+time <- c((10*b):(10*(max(anemia2$obs_time[anemia2$GVHD==1]-b))))/10
+
+# compute smoothed hazard function
+lambda <- vector()
+for(i in 1:length(time)){
+  s <- 0
+  for(j in 1:nrow(anemia2)){
+    x <- (time[i] - anemia2$obs_time[j])/b # compute (t-t_j)/b
+    k <- 3/4*(1-x^2)*(abs(x) <= 1) # plug into kernel equation
+    # (in our case, the Epanechnikov kernel function)
+    h <- anemia2$hazard[j]
+    # change in cumulative hazard is same as just the hazard
+    s <- s + k*h 
+  }
+  lambda[i] <- s/b 
+}
+
+smoothed <- data.frame(time,lambda)
+
+plot10 <- ggplot(data = smoothed, aes(x = time, y = lambda)) + 
+  geom_line() +
+  xlab("Time") +
+  ylab("Hazard Rate") +
+  ggtitle(paste("Smoothed Plot of the Hazard Function with Bandwidth = ", b))
+
+plot10
