70823ae34de43660598f9ef506f8782d6d0ca6f5
discmagnet <kkumbier13@gmail.com>
2018-12-06 00:47:59 -0500

    commit

--- hw4.R
+++ hw4.R
@@ -11,11 +11,23 @@
 library(haven)
 bmt <- read_sas("~/WORKING_DIRECTORIES/biostat.675/bmt_lymphoma_1.sas7bdat")
 
+bmt$Wait_time_yrs <- bmt$Wait_time/12
+
 # (a) Fit a Weibull regression model containing all main effects, with waiting
 #     time measured in years. List the parameter estimates and corresponding
 #     estimated standard errors.
 
+library(survival)
+library(broom)
+model <- survreg(formula = Surv(obs_time,dead) ~ Karnof + 
+                   Wait_time_yrs + factor(Tx_type) + factor(cancer),
+                 data = bmt,
+                 dist = "weibull")
+est <- tidy(model)
+
 # (b) Provide a frequency table for Karnofsky score.
+
+
 
 # (c) Estimate and interpret the Karnofsky effect from a PH perspective; pay
 #     attention to the table you produced in (b). Compute a 95% confidence 
